25a42e1048e9f2fa491d22987427afe2e1009cc0
Abu Rasyid Alfatih <aburasyidalfatih@gmail.com>
2026-05-09 19:08:39 +0700

    style: fix missing horizontal padding on GTK pages for mobile view

--- src/components/layout/gtk-app-layout.tsx
+++ src/components/layout/gtk-app-layout.tsx
@@ -105,7 +105,7 @@ export function GtkAppLayout({ children }: { children: React.ReactNode }) {
       </header>
 
       {/* Main Content */}
-      <main className="flex-1 w-full max-w-7xl mx-auto lg:px-6 lg:py-8 lg:pb-8 pb-28">
+      <main className="flex-1 w-full max-w-7xl mx-auto px-4 py-6 sm:px-6 lg:px-8 lg:py-8 pb-32 lg:pb-8">
         {children}
       </main>
 
